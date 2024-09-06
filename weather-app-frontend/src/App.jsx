405a93fcac90137648282a01bb895cf0415ac3ea
Lautaro Trabajo <lautaro.fuente@yahoo.com>
2024-09-06 14:02:22 -0300

    estructura

---
+++ weather-app-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ViewWeather from "./ViewWeather"
+
+function App() {
+
+
+  return (
+    <>
+      <ViewWeather/>
+    </>
+  )
+}
+
+export default App
---
+++ weather-app-frontend/src/ViewWeather.jsx
@@ -0,0 +1,30 @@
+import { fetchWeatherApi } from 'openmeteo';
+import { useEffect } from 'react';
+
+const params = {
+	"latitude": 52.52,
+	"longitude": 13.41,
+	"hourly": "temperature_2m"
+};
+const url = "https://api.open-meteo.com/v1/forecast";
+
+function ViewWeather() {
+
+    const fetchWeather = async () =>{
+        const responses = await fetchWeatherApi(url, params);
+        console.log(responses);
+    }
+
+    useEffect(() => {
+      fetchWeather();
+    
+
+    }, [])
+    
+
+    return ( <>
+        <h1>Clima</h1>
+    </> );
+}
+
+export default ViewWeather;
