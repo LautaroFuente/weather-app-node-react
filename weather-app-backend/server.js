b69ece722f100158226a6d4647744d7e65717dc9
Lautaro Trabajo <lautaro.fuente@yahoo.com>
2024-10-17 14:40:35 -0300

    finish connection in server.js

--- weather-app-backend/server.js
+++ weather-app-backend/server.js
@@ -1,6 +1,7 @@
 import app from "./app.js";
 import dotenv from "dotenv";
 import process from "process";
+import { connect, closeConnection} from "./configuration/DatabaseConnection.js";
 
 dotenv.config();
 
@@ -8,14 +9,14 @@ const port = process.env.PORT || 3000;
 
 (async () => {
   try {
-
+    await connect();
     app.listen(port, () => {
       console.log(`La aplicación está funcionando en http://localhost:${port}`);
     });
 
     process.on("SIGINT", async () => {
       try {
-
+        await closeConnection();
         console.error("Se ha cerrado la conexion con la base de datos");
         process.exit(0);
       } catch (err) {
